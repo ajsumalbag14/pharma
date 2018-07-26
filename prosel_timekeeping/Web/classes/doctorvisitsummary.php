358ceadafaab9897f1bab1f68fc6e255bed4ecce
Arvin Jay Sumalbag <Arvin Jay Sumalbag@egg.ph>
2018-07-26 13:18:57 +0800

    Patch #2

---
+++ prosel_timekeeping/Web/classes/doctorvisitsummary.php
@@ -0,0 +1,72 @@
+<?php
+
+class DoctorVisit
+{
+    // table name definition and database connection
+    public $db_conn;
+    public $table_name = "doctor_visit";
+
+
+	//object for doctorvisit
+	public $doctorvisitid;
+	public $userid;
+	public $doctorid;
+	public $visitdatetime;
+	public $doctorsignature;
+	public $locationlat;
+	public $locationlong;
+	public $remarks;
+
+	public $query_string;
+	
+	
+    public function __construct($db)
+    {
+        $this->db_conn = $db;
+    }
+
+	function getDoctorsVisits($request)
+	{
+		$user_que = '';
+		$dt_que = '';
+
+		$user_que = $request['user_type'] == 'Administrator' ? " u.USER_ID <> ".$request['parent_user_id'] : " u.PARENT_USER_ID = ".$request['parent_user_id']; 
+		if (isset($request['startdate']) && isset($request['enddate'])) {
+			$dt_que = " AND date(dv.VISIT_DATETIME) BETWEEN '".$request['startdate']."' AND '".$request['enddate']."' ";
+		}
+
+		$sql = "SELECT 
+				dv.DOCTOR_VISIT_ID,
+				CONCAT(u.FIRST_NAME, ' ', u.LAST_NAME) AS 'USER',
+				CONCAT(d.FIRST_NAME, ' ', d.MIDDLE_INITIAL, ' ', d.LAST_NAME) AS 'DOCTOR',
+				dv.VISIT_DATETIME,
+				SUM(NET_PRICE) AS 'TOTAL'
+			FROM doctor_visit dv
+			INNER JOIN doctors d ON d.DOCTOR_ID = dv.DOCTOR_ID
+			INNER JOIN users u ON u.USER_ID = dv.USER_ID
+			INNER JOIN doctor_purchase dp ON dp.DOCTOR_VISIT_ID = dv.DOCTOR_VISIT_ID
+			WHERE 
+			".$user_que.$dt_que;
+
+		$sql .= " GROUP BY dp.DOCTOR_VISIT_ID ORDER BY VISIT_DATETIME ASC;";
+			
+		//echo $sql;
+		$this->query_string = $sql;
+		
+		$prep_state = $this->db_conn->prepare($sql);
+        $prep_state->execute();
+
+        return $prep_state;
+	}
+
+	function getDateDiff($start_date, $end_date) {
+
+		$sd = strtotime($start_date);
+		$ed = strtotime($end_date);
+		$datediff = $ed - $sd;
+
+		$result = (round($datediff / (60 * 60 * 24)) + 1);
+
+		return $result;
+	}
+}
--- prosel_timekeeping/Web/summaryreport/deprecated_index.php
+++ prosel_timekeeping/Web/summaryreport/deprecated_index.php
@@ -1,15 +1,26 @@
 <?php
-	$page_title = 'Doctors Visits Summary';
+	$page_title = 'Area Manager Summary Report';
 	$summary = 'active';
 
 	include_once "../header.php";
-	include_once '../classes/doctorvisitsummary.php';
+	include_once '../classes/_areamanager.php';
 
 	// instantiate database and user object
-	$visits = new DoctorVisit($db);
+	$areamanager = new AreaManager($db);
 
 	$startdate = isset($_POST['startdate']) ? $_POST['startdate'] : date('Y-m-d', strtotime('previous monday'));
-	$enddate = isset($_POST['enddate']) ? $_POST['enddate'] : date('Y-m-d', strtotime('next monday'));
+	$enddate = isset($_POST['enddate']) ? $_POST['enddate'] : date('Y-m-d', strtotime('this friday'));
+	$typ = isset($_POST['typ']) ? $_POST['typ'] : 'All';
+	switch($typ) {
+	case 'API_LOGIN':
+		$typ1 = ''; $typ2 = 'selected'; $typ3 = '';
+		break;
+	case 'API_LOGOUT':
+		$typ1 = ''; $typ2 = ''; $typ3 = 'selected';
+		break;
+	default:
+		$typ1 = 'selected'; $typ2 = ''; $typ3 = '';
+	}
 
 	$date_err = 0;
 ?>
@@ -28,6 +39,13 @@
 							End Date:
 							<input type="date" name="enddate" value="<?php echo $enddate?>" />
 
+							Type: 
+							<select name="typ">
+								<option <?php echo $typ1 ?> value="All">All</option>
+								<option <?php echo $typ2 ?> value="API_LOGIN">Login</option>
+								<option <?php echo $typ3 ?> value="API_LOGOUT">Logout</option>
+							</select>
+
 							<input class="btn btn-primary pull-right" type="submit" value="Filter Record" />
 							
 							
@@ -38,21 +56,23 @@
 
 				<?php
 
-				$datediff = $visits->getDateDiff($startdate, $enddate);
+				$datediff = $areamanager->getDateDiff($startdate, $enddate);
 				$parent_user_id = $_SESSION['USER_ID'];
 				
 				//filter by 7 days result only
 				//if ($datediff >= 0 && $datediff < 8) {
 					// select all users
 					$param = [
-						'user_type'			=> $_SESSION['USER_TYPE'],
 						'parent_user_id'	=> $parent_user_id,
 						'startdate'			=> $startdate,
 						'enddate'			=> $enddate,
-						'datediff'			=> $datediff
+						'datediff'			=> $datediff,
+						'type'				=> isset($_POST['typ']) ? $_POST['typ'] : 'All',
+						'area_id'			=> 0,
+						'area_manager'		=> 0	
 					];
 
-					$prep_state = $visits->getDoctorsVisits($param); //Name of the PHP variable to bind to the SQL statement parameter.
+					$prep_state = $areamanager->getAttendance($param); //Name of the PHP variable to bind to the SQL statement parameter.
 					$num = $prep_state->rowCount();
 					$ctr = 1;
 				//} else {
@@ -68,11 +88,12 @@
 						<thead>
 							<tr>
 								<th>#</th>
-								<th>DOCTOR VISIT ID</th>
-								<th>AREA MANAGER</th>
-								<th>DOCTOR</th>
-								<th>DATE &amp; TIME</th>
-								<th>TOTAL AMOUNT PURCHASED</th>
+								<th>AREA ID</th>
+								<th>NAME</th>
+								<th>DATE TIME</th>
+								<th>TYPE</th>
+								<th>REMARKS</th>
+								<th>:</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -81,6 +102,12 @@
 						if ($num > 0 ){
 							while ($row = $prep_state->fetch(PDO::FETCH_ASSOC)){
 
+								$name = $row["FIRST_NAME"] . " " . $row{"LAST_NAME"};
+								$areaid = $row["AREA_ID"];
+								$dt = $row["ACTIVITY_DATETIME"];
+								$type = str_replace('API_' ,'', $row['ACTIVITY_TYPE']);
+								$remarks = $row["REMARKS"];
+
 								if ($ctr%2 == 0) {
 									echo '<tr class="even gradeX">';
 								} else {
@@ -88,12 +115,17 @@
 								}	
 
 								echo '
-										<td>'.$ctr.'</td>
-										<td>'.$row['DOCTOR_VISIT_ID'].'</td>
-										<td>'.$row['USER'].'</td>
-										<td>'.$row['DOCTOR'].'</td>
-										<td>'.$row['VISIT_DATETIME'].'</td>
-										<td class="center">'.number_format($row['TOTAL'], 2).'</td>
+									<td>'.$ctr.'</td>
+									<td>'.$areaid.'</td>
+									<td>'.$name.'</td>
+									<td>'.$dt.'</td>
+									<td class="center">'.$type.'</td>
+									<td class="center">'.$remarks.'</td>
+									<td>
+										<a href="doctorvisit.php?userid='.$row['USER_ID'].'&date='.$row['dt'].'" class="btn btn-success left-margin">
+											<span class="glyphicon glyphicon-edit"></span> Doctor Visits
+										</a>
+									</td>
 									</tr>
 								';
 
@@ -103,7 +135,7 @@
 							if ($date_err) {
 								echo '
 									<tr class="odd gradeX">
-										<td colspan="5">Date range has reached the limit.</td>
+										<td colspan="5">Date range has reached the limit. Max date range is seven days only.</td>
 									</tr>
 								';
 							} else {
@@ -125,9 +157,9 @@
 					echo '
 						<form action="../export/index.php" method="post">
 							<input type="hidden" name="module" value="'.$page_title.'">
-							<input type="hidden" name="rpt" value="2">
+							<input type="hidden" name="rpt" value="1">
 							<textarea name="query" cols="30" rows="10" style="display:none">
-								'.$visits->query_string.'
+								'.$areamanager->query_string.'
 							</textarea>
 							<input type="submit" class="btn btn-primary" value="Export to Excel">
 						</form>';
