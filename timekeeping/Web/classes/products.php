11242bee3020115c847bd768b332134a303898b5
Arvin Jay Sumalbag <Arvin Jay Sumalbag@egg.ph>
2018-08-14 12:56:40 +0800

    PF#2 - Item 5

--- timekeeping/Web/classes/products.php
+++ timekeeping/Web/classes/products.php
@@ -95,20 +95,15 @@ class Products
         }
     }
 
-    function getAll($from_record_num, $records_per_page)
+    function getAll()
     {		
-		$sql = "SELECT * FROM $this->table_name ORDER BY PRODUCT_CODE ASC LIMIT ?, ?";
+		$sql = "SELECT * FROM $this->table_name ORDER BY PRODUCT_CODE ASC";
 		
         $prep_state = $this->db_conn->prepare($sql);
-
-        $prep_state->bindParam(1, $from_record_num, PDO::PARAM_INT); //Represents the SQL INTEGER data type.
-        $prep_state->bindParam(2, $records_per_page, PDO::PARAM_INT);
-
 
         $prep_state->execute();
 
         return $prep_state;
-        $db_conn = NULL;
     }
 
     // for edit user form when filling up
